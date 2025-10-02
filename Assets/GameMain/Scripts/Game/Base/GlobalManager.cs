79d4bae02da3d9e2a8a8c0bd3eb16afdeb20938b
EqLin <397952780@qq.com>
2025-10-02 21:49:57 +0800

    Steam初始化回调

---
+++ Assets/GameMain/Scripts/Game/Base/GlobalManager.cs
@@ -0,0 +1,20 @@
+﻿using HeathenEngineering.SteamworksIntegration.API;
+using UnityGameFramework.Runtime;
+
+namespace RoundHero
+{
+    public class GlobalManager : TMonoSingleton<GlobalManager>
+    {
+        public bool SteamInitialized = false;
+        
+        public void SteamInitSuccess()
+        {
+            SteamInitialized = true;
+            var user = User.Client.Id;
+            Log.Debug("steam init success name:" + user.Nickname);
+            
+        }
+    }
+    
+    
+}
