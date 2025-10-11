0a2955295d5371332bc314e08755e8bfc8a0e5ab
EqLin <397952780@qq.com>
2025-10-11 10:19:25 +0800

    不要重复设置帧率

--- Assets/GameMain/Scripts/Procedure/ProcedurePreload.cs
+++ Assets/GameMain/Scripts/Procedure/ProcedurePreload.cs
@@ -70,8 +70,7 @@ namespace RoundHero
             GameEntry.Event.Subscribe(LoadDictionaryFailureEventArgs.EventId, OnLoadDictionaryFailure);
             //HeathenEngineering.SteamworksIntegration.API.App.evtSteamInitialized.AddListener(HandleInitialization);
             //HeathenEngineering.SteamworksIntegration.API.App.evtSteamInitializationError.AddListener(HandleInitializationError);
-            Application.targetFrameRate = -1;
-            
+
             m_LoadedFlag.Clear();
 
             PreloadResources();
