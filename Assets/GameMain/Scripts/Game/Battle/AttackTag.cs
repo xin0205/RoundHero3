6a2af78ea53abc451857d117738db007a316434b
EqLin <397952780@qq.com>
2026-01-08 17:16:05 +0800

    整理资源

---
+++ Assets/GameMain/Scripts/Game/Battle/AttackTag.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace RoundHero
+{
+    public class AttackTag : MonoBehaviour
+    {
+        [SerializeField] private Text text;
+        
+        public void SetData(Vector2 pos, int value)
+        {
+            transform.SetParent(AreaController.Instance.BattleFormRoot.transform);
+            transform.localScale = Vector3.one; 
+            
+            text.text = value.ToString();
+
+            //text.color = hurtColor;
+            transform.localPosition = pos;
+        }
+    }
+}
